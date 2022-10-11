86d1d2c097efadea74eec8e0d6dc8379c469a85b
Dusty <martin.70cz@gmail.com>
2022-10-11 19:56:26 +0200

    Přidejte soubory projektu.

---
+++ Ridici_struktury08/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Ridici_struktury08
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                for (int i = 0; i < textBox1.Lines.Count(); i++)
+                {
+                    string radek = textBox1.Lines[i];
+                   
+                    for (int j = 0; j < radek.Length; j++)
+                    {
+                        radek = char.ToUpper(radek[0]) + radek.Substring(1);
+                        textBox1.Text = radek;
+                    }
+                    
+                   
+                     
+
+                }
+             
+            }
+
+            if (checkBox2.Checked == true)
+            {
+                for (int i = 0; i < textBox1.Lines.Count(); i++)
+                {
+                    string radek = textBox1.Lines[i];
+                    for (int j = 0; j < radek.Length; j++)
+                    {
+                        if (!radek.EndsWith("."))
+                        {
+                            radek = radek.Insert(radek[j - 1], ".");
+                    }
+                    }
+
+                   
+
+
+
+                }
+            }
+        }
+    }
+}
